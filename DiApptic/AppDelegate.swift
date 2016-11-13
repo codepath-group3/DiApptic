6fdd1d4f7137c527524211a71783c80bd7878e03
Neha Samant <nehaa1812@gmail.com>
2016-11-13 15:18:59 -0800

    adding parse pod

--- DiApptic/AppDelegate.swift
+++ DiApptic/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("cuRMkMTRwxGTYVofphrR5ROWEBVJIF8pYYyTLcM3", clientKey: "BFkrS2bmejYuP5895kBffPpjI3JJFpPIfjn0ZHHG")
         return true
     }
 
--- Podfile
+++ Podfile
@@ -4,7 +4,8 @@
 target 'DiApptic' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
-
+  pod 'AFNetworking'
+  pod 'Parse'
   # Pods for DiApptic
 
 end
