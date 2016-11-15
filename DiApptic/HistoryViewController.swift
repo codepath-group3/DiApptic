a44eeb72d0d3243ec550ebc2545030b1004e8e32
Faheem Hussain <fhussain@servicesource.com>
2016-11-14 19:50:22 -0800

    Add Tabbar controller

--- DiApptic/HistoryViewController.swift
+++ DiApptic/HistoryViewController.swift
@@ -7,29 +7,69 @@
 //
 
 import UIKit
+import Charts
 
-class HistoryViewController: UIViewController {
+class HistoryViewController: UIViewController, ChartViewDelegate {
+    
+    @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var pieChartView: PieChartView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        edgesForExtendedLayout = []
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //lineChartView.description = " "
+        //pieChartView.description = " "
+        lineChartView.delegate = self
+        lineChartView.chartDescription?.text = "Line Chart"
+        pieChartView.chartDescription?.text = "Pie Chart"
+        let months = ["Jan", "Feb", "Mar"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0]
+        let readings = [200.0, 100.0, 5.0]
+        setChart(dataPoints: months, values: readings)
+        
     }
     
+    func setChart(dataPoints: [String], values: [Double]) {
+        
+        var dataEntries: [ChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(x: values[i], y: Double(i))
+            dataEntries.append(dataEntry)
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let pieChartDataSet = PieChartDataSet(values: dataEntries, label: "Readings")
+        //let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
+        let pieChartData = PieChartData(dataSet: pieChartDataSet)
+        pieChartView.data = pieChartData
+        
+        var colors: [UIColor] = []
+        
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            
+            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
+            colors.append(color)
+        }
+        
+        pieChartDataSet.colors = colors
+        
+        
+        let lineChartDataSet = LineChartDataSet(values: dataEntries, label: "Readings")
+        //let lineChartData = LineChartData(xVals: dataPoints, dataSet: lineChartDataSet)
+        let lineChartData = LineChartData(dataSet: lineChartDataSet)
+        lineChartView.data = lineChartData
+        
     }
-    */
-
+    
+    func chartTranslated(_ chartView: ChartViewBase, dX: CGFloat, dY: CGFloat) {
+        print("chart translated")
+    }
+    func chartScaled(_ chartView: ChartViewBase, scaleX: CGFloat, scaleY: CGFloat) {
+        print("chart scaled")
+    }
 }
