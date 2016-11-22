b1b9d31c94d91f8033006a71792a083b93c8cf4b
Neha Samant <nehaa1812@gmail.com>
2016-11-21 22:05:50 -0800

    fixed error messages on login and register screen

--- DiApptic/RegisterViewController.swift
+++ DiApptic/RegisterViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Parse
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var usernameField: UITextField!
@@ -18,22 +18,60 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var firstnameField: UITextField!
     @IBOutlet weak var lastnameField: UITextField!
     @IBOutlet weak var professionField: UITextField!
-    
+    @IBOutlet weak var inlineUsernameError: UILabel!
+    @IBOutlet weak var inlineLastnameError: UILabel!
+    @IBOutlet weak var inlineFirstnameError: UILabel!
+    @IBOutlet weak var inlinePasswordError: UILabel!
+    @IBOutlet weak var inlineEmailError: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        usernameField.delegate = self
+        password.delegate = self
+        emailField.delegate = self
+        firstnameField.delegate = self
+        lastnameField.delegate = self
     }
 
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        inlineEmailError.isHidden = true
+        inlinePasswordError.isHidden = true
+        inlineUsernameError.isHidden = true
+        inlineFirstnameError.isHidden = true
+        inlineLastnameError.isHidden = true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
     @IBAction func onRegister(_ sender: UIButton) {
+        checkForErrors()
         registerUser()
+    }
+    
+    func checkForErrors() {
+        if((usernameField.text?.characters.count)! < 4) {
+            inlineUsernameError.isHidden = false
+            inlineUsernameError.text = "Username must be greater than 5 characters "
+        }
+        if((password.text?.characters.count)! < 4) {
+            inlinePasswordError.isHidden = false
+            inlinePasswordError.text = "Password must be greater than 6 characters "
+        }
+        if((emailField.text?.characters.count)! < 8) {
+            inlineEmailError.isHidden = false
+            inlineEmailError.text = "Email must be greater than 8 characters "
+        }
+        if((firstnameField.text?.characters.count)! < 3) {
+            inlineFirstnameError.isHidden = false
+            inlineFirstnameError.text = "Firstname must be greater than 3 characters "
+        }
+        if((lastnameField.text?.characters.count)! < 2) {
+            inlineLastnameError.isHidden = false
+            inlineLastnameError.text = "Last name must be greater than 2 characters "
+        }
     }
 
     func registerUser() {
